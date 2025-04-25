8fefba7259e331b5490e719275cf2b7162b8cdc4
Vander Tavares <vander.tavares@youxgroup.com.br>
2025-04-25 15:10:16 -0300

    Project created

---
+++ src/main/java/com/vandertavares/vandin/VandinApplication.java
@@ -0,0 +1,13 @@
+package com.vandertavares.vandin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VandinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VandinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vandertavares/vandin/VandinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vandertavares.vandin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VandinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
