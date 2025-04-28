dea7988b949a86b4e70793a727471e16c644815c
Vander Tavares <vander.tavares@youxgroup.com.br>
2025-04-28 11:53:09 -0300

    Service layer, component registration

--- src/main/java/com/vandertavares/vandin/repositories/UserRepository.java
+++ src/main/java/com/vandertavares/vandin/repositories/UserRepository.java
@@ -4,7 +4,4 @@ import com.vandertavares.vandin.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
-
-
 }
---
+++ src/main/java/com/vandertavares/vandin/service/UserService.java
@@ -0,0 +1,25 @@
+package com.vandertavares.vandin.service;
+
+import com.vandertavares.vandin.entities.User;
+import com.vandertavares.vandin.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
