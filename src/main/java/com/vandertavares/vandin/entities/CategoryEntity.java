443bba8e1cfafc95a1b7c738f7ed0170f7fe91ec
Vander Tavares <vander.tavares@youxgroup.com.br>
2025-04-29 15:40:55 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/vandertavares/vandin/entities/CategoryEntity.java
+++ src/main/java/com/vandertavares/vandin/entities/CategoryEntity.java
@@ -1,5 +1,6 @@
 package com.vandertavares.vandin.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -17,7 +18,8 @@ public class CategoryEntity implements Serializable {
     private Long id;
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     Set<ProductEntity> products = new HashSet<>();
 
     public CategoryEntity(){
